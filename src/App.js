600661319d86c9e25511b3f7303dc0ab7636ecc9
Euphoria99 <363pavanbhat@gmail.com>
2023-09-17 12:35:18 +0530

    Re-Initialized with tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './index.css'
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-3xl font-bold underline">
       Test
     </div>
   );
